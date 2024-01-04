7d169610925a1a84002176d5e28f372f33e37c2e
Dan1elG94 <101414693+Dan1elG94@users.noreply.github.com>
2024-01-04 17:36:23 +0100

    commit builded page

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Contact from './Contact';
-import Events from './Events';
-import Features from './Features';
-import Hero from './Hero';
-import Navbar from './Navbar';
-import MobileMenu from './MobileMenu';
-import Footer from './Footer';
-
-export { Contact, Events, Features, Hero, Navbar, MobileMenu, Footer };
